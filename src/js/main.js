2ffd1197409f235150bd6584bb46ba38df5824c5
Remzov <remzovnik@gmail.com>
2017-12-14 15:27:13 +0300

    Landing work #3

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,31 @@
 // import UIkit from 'uikit';
 // import Icons from 'uikit/dist/js/uikit-icons';
 // UIkit.use(Icons);
+
+let togglerUsman = document.querySelector(".js-toggler-usman");
+let togglerOtradnoe = document.querySelector(".js-toggler-otradnoe");
+let mapUsman = document.querySelector(".map__usman");
+let mapOtradnoe = document.querySelector(".map__otradnoe");
+
+togglerUsman.onclick = function() {
+    mapUsman.removeAttribute('hidden','');
+    mapOtradnoe.setAttribute('hidden','');
+
+    togglerUsman.classList.remove("toggler-usman-unactive");
+    togglerUsman.classList.add("toggler-usman-active");
+
+    togglerOtradnoe.classList.remove("toggler-otradnoe-active");
+    togglerOtradnoe.classList.add("toggler-otradnoe-unactive");
+}
+
+togglerOtradnoe.onclick = function() {
+    mapOtradnoe.removeAttribute('hidden','');
+    mapUsman.setAttribute('hidden','');
+
+    togglerUsman.classList.remove("toggler-usman-active");
+    togglerUsman.classList.add("toggler-usman-unactive");
+
+    togglerOtradnoe.classList.remove("toggler-otradnoe-unactive");
+    togglerOtradnoe.classList.add("toggler-otradnoe-active");
+}
+
