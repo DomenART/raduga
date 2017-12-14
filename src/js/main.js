efec24ac7910cbd2a65ebe470fc14f63379872ec
Remzov <remzovnik@gmail.com>
2017-12-14 17:47:14 +0300

    landing work #4

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@
 // import Icons from 'uikit/dist/js/uikit-icons';
 // UIkit.use(Icons);
 
+
+// Переключение карт на главной
 let togglerUsman = document.querySelector(".js-toggler-usman");
 let togglerOtradnoe = document.querySelector(".js-toggler-otradnoe");
 let mapUsman = document.querySelector(".map__usman");
@@ -14,10 +16,8 @@ let mapOtradnoe = document.querySelector(".map__otradnoe");
 togglerUsman.onclick = function() {
     mapUsman.removeAttribute('hidden','');
     mapOtradnoe.setAttribute('hidden','');
-
     togglerUsman.classList.remove("toggler-usman-unactive");
-    togglerUsman.classList.add("toggler-usman-active");
-
+    togglerUsman.classList.add("toggler-usman-active")
     togglerOtradnoe.classList.remove("toggler-otradnoe-active");
     togglerOtradnoe.classList.add("toggler-otradnoe-unactive");
 }
@@ -25,11 +25,21 @@ togglerUsman.onclick = function() {
 togglerOtradnoe.onclick = function() {
     mapOtradnoe.removeAttribute('hidden','');
     mapUsman.setAttribute('hidden','');
-
     togglerUsman.classList.remove("toggler-usman-active");
     togglerUsman.classList.add("toggler-usman-unactive");
-
     togglerOtradnoe.classList.remove("toggler-otradnoe-unactive");
     togglerOtradnoe.classList.add("toggler-otradnoe-active");
 }
 
+//Валидация форм
+let enrollForm = document.querySelector("#enroll");
+
+enrollForm.addEventListener("ke")
+
+
+
+    let inputs = enrollForm.querySelectorAll("input, textarea");
+    inputs.forEach(element => {
+        element.classList.add('validity');
+    }); 
+}
