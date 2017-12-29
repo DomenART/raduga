5cfb2db0c4cfd50f46431734750ba9f7f83c8a1f
Remzov <remzovnik@gmail.com>
2017-12-29 17:07:40 +0300

    landings fixes

--- src/js/main.js
+++ src/js/main.js
@@ -69,6 +69,30 @@ if (enrollForm) {
 
 /* написать продолжение под внутренние страницы! */
 
+//Кнопка наверх
+let toTop = document.querySelector('.to-top');
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 200) {
+        toTop.hidden = false;
+    } else {
+        toTop.hidden = true;
+    }
+});
+
+function scrollToTop(scrollDuration) {
+    var scrollDuration = 1000;
+    let scrollStep = -window.scrollY / (scrollDuration / 15),
+    scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy(0, scrollStep);
+        }
+        else clearInterval(scrollInterval); 
+    }, 15);
+};
+
+toTop.addEventListener('click', scrollToTop);
+
 //Работа меню
 let menu = document.querySelector('.menu');
 let menuButtons = document.querySelectorAll('.button-menu');
@@ -119,6 +143,7 @@ menuButtons.forEach(element => {
 
 //Включить анимации первого экрана главной
 let homepage = document.querySelector('.homepage');
+let menuBarLogo = document.querySelector('.menu-bar__logo');
 
 if (homepage) {
     window.addEventListener('load', function() {
@@ -128,44 +153,55 @@ if (homepage) {
         let introMajorDivider = document.querySelector('.intro-nav__dot-vertical');
         let introMajorLast = document.querySelector('.intro-nav__major li:last-child');
         let introMenuItems = document.querySelectorAll('.intro-nav__minor li');
-        let menuBarThreshold = document.querySelector('.menu-bar__threshold');
+        let menuBarThresholdInner = document.querySelector('.menu-bar__threshold');
         let menuBarDivider = document.querySelector('.menu-bar__divider');
-        let menuBarLogo = document.querySelector('.menu-bar__logo');
+        
         introLogo.style.animation = '1s appereance forwards';
         introLogo.addEventListener('animationend', function() {
             if ((!menuBar.classList.contains('menu-bar-fixed')) && (!window.matchMedia('(max-width: 639px)').matches)) {
-                menuBar.style.height = '695px';
-            } else {
-                menuBarText.style.opacity = '1';
-            }
-            setTimeout(function() {
-                menuBar.style.height = 'auto';
-                menuBarText.style.opacity = '1';
-            }, 500);
-            setTimeout(function() {
-                menuBarThreshold.style.animation = '1s font-scale';
-                menuBarThreshold.addEventListener('animationend', function() {
-                    menuBarThreshold.style.animation = '';
-                });
-            }, 1500);
-            setTimeout(function() {
-                introMajorFirst.style.animation = '1s slide-ltr forwards';
-            }, 2000);
-            setTimeout(function() {
-                introMajorDivider.style.animation = '1s slide-up forwards';
-            }, 2500);
-            setTimeout(function() {
-                introMajorLast.style.animation = '1s slide-ltr forwards';
-            }, 3000);
-            setTimeout(function() {
-                introMenuItems.forEach(function(item, i, introMenuItems) {
+                menuBar.style.opacity = '1';
+                menuBar.style.animation = '2s menu-bar-appereance';
                     setTimeout(function() {
-                        item.style.animation = '0.5s slide-ltr forwards';
-                    }, i*500);
+                        menuBar.style.animation = '2s menu-bar-rotate 0.5s';
+                        menuBarText.style.animation = '1s appereance 2s forwards';
+                        menuBarText.addEventListener('animationend', function(){
+                            menuBarText.style.opacity = '1';
+                            menuBarText.style.animation = '';
+                        })
+                        menuBarThresholdInner.style.animation = '1s font-scale 3s';
+                        menuBarThresholdInner.addEventListener('animationend', function(){
+                            menuBarThresholdInner.style.animation = '';
+                        })
+                    }, 500);
+                }
+            menuBar.addEventListener('animationend', function() {
+                setTimeout(function() {
+                    introMajorFirst.style.animation = '1s slide-ltr forwards';
+                }, 1000);
+                setTimeout(function() {
+                    if (window.matchMedia('(max-width: 1199px)').matches) {
+                        introMajorDivider.style.animation = '1s slide-up-divider-xsmall forwards';
+                    } else {
+                        introMajorDivider.style.animation = '1s slide-up forwards';
+                    }
+                    introMajorDivider.addEventListener('animationend', function(){
+                        introMajorDivider.style.animation = '';
+                        introMajorDivider.style.opacity = '1';
+                    })
+                }, 2000);
+                setTimeout(function() {
+                    introMajorLast.style.animation = '1s slide-ltr forwards';
+                }, 3000);
+                setTimeout(function() {
+                    introMenuItems.forEach(function(item, i, introMenuItems) {
+                        setTimeout(function() {
+                            item.style.animation = '0.5s slide-ltr forwards';
+                        }, i*500);
+                    });
+                }, 4000);
+                introMenuItems[introMenuItems.length-1].addEventListener('animationend', function() {
+                    menuHeaderButton.style.transform = 'scale(1)';
                 });
-            }, 3500);
-            introMenuItems[introMenuItems.length-1].addEventListener('animationend', function() {
-                menuHeaderButton.style.transform = 'scale(1)';
             });
         });
     });
@@ -192,11 +228,11 @@ if (homepage) {
                 videoalbumImage.style.animation = '1s slide-rtl 2.5s forwards';
                 videoalbumTitle.style.animation = '1s slide-rtl 3s forwards';
                 animated = true;
-            }
+            };
         });
-    }
+    };
 
-    let costumedGirl = document.querySelector('.program__costumedgirl img');
+    let costumedGirl = document.querySelector('.program__costumedgirl');
     
     if (costumedGirl) {
         window.addEventListener('scroll', function() {
@@ -263,7 +299,7 @@ if (homepage) {
             togglerUsman.classList.add('toggler-usman-active')
             togglerOtradnoe.classList.remove('toggler-otradnoe-active');
             togglerOtradnoe.classList.add('toggler-otradnoe-unactive');
-        }
+        };
         togglerOtradnoe.onclick = function() {
             mapOtradnoe.hidden = false;
             mapUsman.hidden = true;0
@@ -275,13 +311,13 @@ if (homepage) {
     }
 }
 
+let menuBarThreshold = document.querySelector('.menu-bar__threshold');
+
 //адаптация под экраны меньше 639px
 if (window.matchMedia('(max-width: 639px)').matches) {
-    let menuBarThreshold = document.querySelector('.menu-bar__threshold');
-    let menuBarDivider = document.querySelector('.menu-bar__divider');
-    let menuBarLogo = document.querySelector('.menu-bar__logo');
-    let introContainer = document.querySelector('.intro__container');
     
+    let menuBarDivider = document.querySelector('.menu-bar__divider');
+    let introContainer = document.querySelector('.intro__container');
     let programIntro = document.querySelector('.programm__intro');
     let programContainer = document.querySelector('.js-program-container');
     let programList = document.querySelector('.program__list');
@@ -311,108 +347,7 @@ if (window.matchMedia('(max-width: 639px)').matches) {
     });
 }
 
-//Кнопка наверх
-let toTop = document.querySelector('.to-top');
-
-window.addEventListener('scroll', function() {
-    if (window.pageYOffset > 20) {
-        toTop.hidden = false;
-    } else {
-        toTop.hidden = true;
-    }
-});
-
-function scrollToTop(scrollDuration) {
-    var scrollDuration = 1000;
-    let scrollStep = -window.scrollY / (scrollDuration / 15),
-    scrollInterval = setInterval(function(){
-        if ( window.scrollY != 0 ) {
-            window.scrollBy(0, scrollStep);
-        }
-        else clearInterval(scrollInterval); 
-    }, 15);
-};
-
-toTop.addEventListener('click', scrollToTop);
-   
-
-/*
 //Линии
-
-let lines = document.querySelectorAll('.line');
-
-let line1 = document.querySelector('.line-1');
-let line2 = document.querySelector('.line-2');
-let line3 = document.querySelector('.line-3');
-let line4 = document.querySelector('.line-4');
-let line5 = document.querySelector('.line-5');
-let line6 = document.querySelector('.line-6');
-let line7 = document.querySelector('.line-7');
-let line8 = document.querySelector('.line-8');
-let line9 = document.querySelector('.line-9');
-let line10 = document.querySelector('.line-10');
-
-window.addEventListener('scroll', function() {
-    let line1Top = line1.getBoundingClientRect().top;
-    if (line1Top < (document.documentElement.clientHeight / 2)) {
-        line1.style.animation = '2s line-1-draw forwards';
-    };
-    line1.addEventListener('animationend', function() {
-        let line2Top = line1.getBoundingClientRect().top;
-        if (line2Top < (document.documentElement.clientHeight / 2)) {
-            line2.style.animation = '2s line-2-draw forwards';
-        };
-    });
-    line2.addEventListener('animationend', function() {
-        let line3Top = line1.getBoundingClientRect().top;
-        if (line3Top < (document.documentElement.clientHeight / 2)) {
-            line3.style.animation = '2s line-3-draw forwards';
-        };
-    });
-    let line4Top = line1.getBoundingClientRect().top;
-    if (line4Top < (document.documentElement.clientHeight / 2)) {
-        line4.style.animation = '2s line-4-draw forwards';
-    };
-    line4.addEventListener('animationend', function() {
-        let line5Top = line1.getBoundingClientRect().top;
-        if (line5Top < (document.documentElement.clientHeight / 2)) {
-            line5.style.animation = '2s line-5-draw forwards';
-        };
-    });
-    line5.addEventListener('animationend', function() {
-        let line6Top = line1.getBoundingClientRect().top;
-        if (line6Top < (document.documentElement.clientHeight / 2)) {
-            line6.style.animation = '2s line-6-draw forwards';
-        };
-    });
-    line6.addEventListener('animationend', function() {
-        let line7Top = line1.getBoundingClientRect().top;
-        if (line7Top < (document.documentElement.clientHeight / 2)) {
-            line7.style.animation = '2s line-7-draw forwards';
-        };
-    });
-    line7.addEventListener('animationend', function() {
-        let line8Top = line1.getBoundingClientRect().top;
-        if (line8Top < (document.documentElement.clientHeight / 2)) {
-            line8.style.animation = '2s line-8-draw forwards';
-        };
-    });
-    line8.addEventListener('animationend', function() {
-        let line9Top = line1.getBoundingClientRect().top;
-        if (line9Top < (document.documentElement.clientHeight / 2)) {
-            line9.style.animation = '2s line-9-draw forwards';
-        };
-    });
-    line9.addEventListener('animationend', function() {
-        let line10Top = line1.getBoundingClientRect().top;
-        if (line10Top < (document.documentElement.clientHeight / 2)) {
-            line10.style.animation = '2s line-10-draw forwards';
-        };
-    });
-});
-*/
-
-        
 let canvasHomepage = document.querySelector('#canvasHomepage');
 
 canvasHomepage.setAttribute('width', document.documentElement.offsetWidth);
@@ -428,7 +363,7 @@ function getCoords(elem) {
     };
 }
 
-if (canvasHomepage.getContext) {
+if ((canvasHomepage.getContext) && (window.matchMedia('(min-width: 639px)').matches)) {
     let controller = new ScrollMagic.Controller();
     var ctx = canvasHomepage.getContext('2d');
     let xDraw, yDraw
@@ -451,7 +386,7 @@ if (canvasHomepage.getContext) {
     
     let start = {
         x: 0,
-        y: tutorsImageCoords.top + (tutorsImage.offsetHeight * 0.1)
+        y: tutorsImageCoords.top + tutorsImage.offsetHeight * 0.1
     }
     let offset = start.y - window.innerHeight * 0.7
     let defaultDuration = window.innerHeight * 0.3
